3e66d21564e40684b5ad5a1c3e33d86f735923f2
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2017-02-24 14:59:56 -0800

    mm/thp/autonuma: use TNF flag instead of vm fault
    We are using the wrong flag value in task_numa_falt function.  This can
    result in us doing wrong numa fault statistics update, because we update
    num_pages_migrate and numa_fault_locality etc based on the flag argument
    passed.
    
    Fixes: bae473a423 ("mm: introduce fault_env")
    Link: http://lkml.kernel.org/r/1487498395-9544-1-git-send-email-aneesh.kumar@linux.vnet.ibm.com
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Acked-by: Hillf Danton <hillf.zj@alibaba-inc.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -1328,7 +1328,7 @@ out:
 		page_unlock_anon_vma_read(anon_vma);
 
 	if (page_nid != -1)
-		task_numa_fault(last_cpupid, page_nid, HPAGE_PMD_NR, fe->flags);
+		task_numa_fault(last_cpupid, page_nid, HPAGE_PMD_NR, flags);
 
 	return 0;
 }
