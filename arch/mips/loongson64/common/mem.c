kernelorg-mirror/linux_kernel_git_vapier_lemote

Loongson: Add memory type UMA_VIDEO_RAM to pass shared VRAM info
Signed-off-by: Huacai Chen <chenhc@lemote.com>

arch/mips/loongson/common/mem.c
@@ -93,6 +93,14 @@ void __init prom_init_memory(void)
 				add_memory_region(emap->map[i].mem_start,
 					0x2000, BOOT_MEM_RESERVED);
 				break;
+			case UMA_VIDEO_RAM:
+				vram_type = VRAM_TYPE_UMA;
+				uma_vram_addr = emap->map[i].mem_start & 0xffffffff;
+				uma_vram_size = emap->map[i].mem_size;
+				add_memory_region(emap->map[i].mem_start,
+					(u64)emap->map[i].mem_size << 20,
+					BOOT_MEM_RESERVED);
+				break;
 			}
 		}
 	}

lshw/linux-loongson_lemote

arch/mips/loongson64/common/mem.c
@@ -97,6 +97,14 @@ void __init prom_init_memory(void)
 				add_memory_region(loongson_memmap->map[i].mem_start,
 					0x2000, BOOT_MEM_RESERVED);
 				break;
+			case UMA_VIDEO_RAM:
+				loongson_sysconf.vram_type = VRAM_TYPE_UMA;
+				loongson_sysconf.uma_vram_addr = loongson_memmap->map[i].mem_start & 0xffffffff;
+				loongson_sysconf.uma_vram_size = loongson_memmap->map[i].mem_size;
+				add_memory_region(loongson_memmap->map[i].mem_start,
+					(u64)loongson_memmap->map[i].mem_size << 20,
+					BOOT_MEM_RESERVED);
+				break;
 			}
 		}
 	}